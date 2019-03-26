6d7656729c2511ab168d2bedffe091663cb22942
jhunsley <john.hunsley@capgemini.com>
2019-03-26 11:42:20 +0000

    initial project structure

---
+++ src/main/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PagerContainer from './components/PagerContainer';
+
+const App = () => <PagerContainer/>
+
+export default App;
---
+++ src/main/components/PagerContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PagerContainer extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <span>Its a pager, woo!</span>
+            </div>
+
+        )
+    }
+
+}
+
+export default PagerContainer;
